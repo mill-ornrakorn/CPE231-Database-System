a8a9daa886930ebe3cdbbf779b58ee165cc35da5
Papin Thanutchapat <99426328+Jappapin@users.noreply.github.com>
2022-02-14 20:57:39 +0700

    Update settings.py

--- NETFLEX final project/final_project/settings.py
+++ NETFLEX final project/final_project/settings.py
@@ -20,9 +20,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'j$_eq^d2_2h=p=$ly3hy&0**8dv3f+=zb+otqjnqgqe8a@lrvo'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
